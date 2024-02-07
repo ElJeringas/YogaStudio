643d7c0b31e6d7cb0ad3c8a8cbbd37f60606d9a9
ElJeringas <santiagojss32@gmail.com>
2024-02-07 10:37:58 -0500

    New Logo, images and colours

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
         <div className='header__content'>
           <h1>Psicologia<br/><span>Holistica</span></h1>
           {/* <h2>Holistica</h2> */}
-          <a type='button' href='https://api.whatsapp.com/send/?phone=593996819305&text&type=phone_number&app_absent=0' target="_blank" rel="noopener noreferrer" className='btn' style={{marginTop:'3rem'}}>Contactame</a>
+          <a type='button' href='https://api.whatsapp.com/send/?phone=593996819305&text&type=phone_number&app_absent=0' target="_blank" rel="noopener noreferrer" className='btn' >Contactame</a>
         </div>
         <div className='header__image'>
           <img src={images.home} alt='header'/>
